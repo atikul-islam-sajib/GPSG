51d69a84e4b4ffb7c54fc66a6f4fd6a3f09b2604
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-01-21 21:50:40 +0100

    init file is added

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="simple_gan",
+    version="0.1.0",  # Update with the version number of your package
+    description="A deep learning project that is build for predicting the Mnist dataset",
+    author="Atikul Islam Sajib",
+    author_email="atikul.sajib@ptb.de",
+    url="hhttps://github.com/atikul-islam-sajib/GoodPractiseDSID",  # Update with your project's GitHub repository URL
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.9",
+    ],
+    keywords="Alzheimer classification machine-learning",
+    project_urls={
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/GoodPractiseDSTD/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/GoodPractiseDSTD/blob/main/README.md",
+        "Source Code": "https://github.com/atikul-islam-sajib/GoodPractiseDSTD.git",
+    },
+)
