bd1a27ce52fb3f266b974305eaca650a894e5e82
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-01-24 22:57:43 +0100

    requirements file is updated

--- clf.py
+++ clf.py
@@ -1,5 +1,11 @@
 import torch
 import sklearn
+import matplotlib
+import seaborn
+import pandas
 
 print(torch.__version__)
 print(sklearn.__version__)
+print(matplotlib.__version__)
+print(seaborn.__version__)
+print(pandas.__version__)
