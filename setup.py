95658a8e3380cb522aa256b81e963f867f60f5ca
Atikul Islam Sajib <atikulislamsajib137@gmail.com>
2024-01-25 17:28:57 +0100

    setup file is updated

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="simple_gan",
     version="0.1.0",  # Update with the version number of your package
-    description="A deep learning project that is build for predicting the Mnist dataset",
+    description="A deep learning project that is build for GAN for the Mnist dataset",
     author="Atikul Islam Sajib",
     author_email="atikul.sajib@ptb.de",
-    url="hhttps://github.com/atikul-islam-sajib/GoodPractiseDSID",  # Update with your project's GitHub repository URL
+    url="https://github.com/atikul-islam-sajib/GPSG.git",  # Update with your project's GitHub repository URL
     packages=find_packages(),
     classifiers=[
         "Development Status :: 3 - Alpha",
@@ -16,8 +16,8 @@ setup(
     ],
     keywords="Alzheimer classification machine-learning",
     project_urls={
-        "Bug Tracker": "https://github.com/atikul-islam-sajib/GoodPractiseDSTD/issues",
-        "Documentation": "https://github.com/atikul-islam-sajib/GoodPractiseDSTD/blob/main/README.md",
-        "Source Code": "https://github.com/atikul-islam-sajib/GoodPractiseDSTD.git",
+        "Bug Tracker": "https://github.com/atikul-islam-sajib/GPSG.git/issues",
+        "Documentation": "https://github.com/atikul-islam-sajib/GPSG.git/blob/main/README.md",
+        "Source Code": "https://github.com/atikul-islam-sajib/GPSG.git",
     },
 )
